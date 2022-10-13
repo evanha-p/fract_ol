48418faddb9ce63e45464187775c9432a4811d73
Eero Vanha-Perttula <eero.vanhaperttula@gmail.com>
2022-10-13 19:08:52 +0300

    initial commit. Added libft, makefile, .h file and fractals.c. Nothing working yet

---
+++ Makefile
@@ -0,0 +1,78 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/10/07 18:23:20 by evanha-p          #+#    #+#              #
+#    Updated: 2022/10/13 18:37:15 by evanha-p         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME			=	fractol
+
+H_FOLDER		=	includes
+C_FOLDER		=	srcs
+OBJ_FOLDER		=	obj
+FOLDER_LIST		=	$(H_FOLDER) $(C_FOLDER) $(OBJ_FOLDER)
+
+H_FILES			=	$(NAME).h
+C_FILES			=	
+
+H_PATHS			=	$(addprefix $(H_FOLDER)/, $(H_FILES))
+C_PATHS			=	$(addprefix $(C_FOLDER)/, $(C_FILES))
+OBJ_PATHS		=	$(addprefix $(OBJ_FOLDER)/, $(patsubst %.c, %.o, $(C_FILES)))
+
+LINKS			=	-I /usr/local/include -L /usr/local/lib -lmlx -framework \
+					OpenGL -framework Appkit
+C_FLAGS			=	-Wall -Wextra -Werror
+FT_INC			=	-I libft/includes
+FT_LIB			= 	libft/libft.a
+
+.PHONY: all
+all: $(NAME)
+
+$(NAME): pre_requisites $(OBJ_PATHS)
+	@touch pre_requisites
+	@make -C libft
+	cc $(C_FLAGS) -I $(H_FOLDER) $(FT_LIB) $(LINKS) -o $@ $(OBJ_PATHS)
+
+$(OBJ_PATHS): $(OBJ_FOLDER)/%.o:$(C_FOLDER)/%.c $(H_PATHS)
+	cc $(C_FLAGS) $(FT_INC) -I $(H_FOLDER) -c $< -o $@
+
+pre_requisites: $(FOLDER_LIST) $(H_PATHS) $(C_PATHS)
+
+$(FOLDER_LIST):
+	mkdir $@
+
+$(H_PATHS):
+	touch $@
+
+$(C_PATHS):
+	touch $@
+
+.PHONY: print_files
+print_files:
+	@echo "H paths:   $(H_PATHS)"
+	@echo "C paths:   $(C_PATHS)"
+	@echo "OBJ paths: $(OBJ_PATHS)"
+	@echo "C flags:   $(C_FLAGS)"
+	@echo ""
+
+.PHONY: clean
+clean:
+	-@rm -f $(OBJ_PATHS)
+	-@make clean -C libft
+	@echo "Object files removed, squeaky clean!"
+
+.PHONY: fclean
+fclean: clean
+	-@rm -f $(NAME)
+	-@rm -fd $(OBJ_FOLDER)
+	-@rm -f pre_requisites
+	@make fclean -C libft
+	@echo "$(NAME) and object files removed. Nice!"
+
+.PHONY: re
+re: fclean all
---
+++ libft/srcs/get_next_line.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evanha-p <evanha-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 17:57:41 by evanha-p          #+#    #+#             */
+/*   Updated: 2022/07/07 13:46:52 by evanha-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* Saves what we have in variable saved to variable line.
+ * First we check where the '\n' if there is one.
+ * We save everything before the first '\n' to line and the
+ * rest to variable temp. After this we clear and free saved pointer
+ * and set the pointer to temp and return 1.
+ *
+ * If we don't find '\n' we simply dublicate everything to line
+ * and clear and free variable saved. We return 1 if there is
+ * still something in variable line. Otherwise we return 0
+ * meaning we have finished reading the file. */
+
+int	ft_savestring(char **line, char **saved, int fd, int ret)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	if (ft_strchr(saved[fd], '\n'))
+	{
+		while (saved[fd][i] != '\n')
+			i++;
+		*line = ft_strsub(saved[fd], 0, i);
+		temp = ft_strsub(saved[fd], (i + 1), (ft_strlen(saved[fd]) - i));
+		ft_strclr(saved[fd]);
+		ft_strdel(&saved[fd]);
+		saved[fd] = temp;
+		return (1);
+	}
+	*line = ft_strdup(saved[fd]);
+	ft_strclr(saved[fd]);
+	ft_strdel(&saved[fd]);
+	if (ret == 0)
+		return (0 + (**line != 0));
+	return (1);
+}
+
+/* Combines what we already have stored
+ * in variable saved and what we read from buffer.
+ * It stores it to temp returning it and clears
+ * the variable saved. */
+
+void	ft_joinandclean(char **saved, char *buffer, int fd)
+{
+	char	*temp;
+
+	temp = ft_strjoin(saved[fd], buffer);
+	ft_strclr(saved[fd]);
+	ft_strdel(&saved[fd]);
+	saved[fd] = temp;
+}
+
+/* The main function. First we check for errors and if
+ * there is already something saved that includes '\n'. If
+ * so we don't have to read again so we skip that part and
+ * jump straight to ft_savestring.
+ * In the while loop we read until we:
+ * a) reach the end of file
+ * b) find '\n'
+ * In lines 76 to 79 we check if we already have something saved
+ * and react respectively. If we don't we duplicate, if we do,
+ * we combine the strings */
+
+int	get_next_line(const int fd, char **line)
+{
+	static char	*saved[GNL_MAX_FD];
+	char		buffer[GNL_BUFF_SIZE + 1];
+	int			ret;
+
+	ret = 1;
+	if (fd < 0 || fd > GNL_MAX_FD || !line)
+		return (-1);
+	if (saved[fd] && ft_strchr(saved[fd], '\n'))
+		return (ft_savestring(line, saved, fd, ret));
+	while (ret)
+	{
+		ret = read(fd, buffer, GNL_BUFF_SIZE);
+		if (ret < 0)
+			return (-1);
+		buffer[ret] = '\0';
+		if (!saved[fd])
+			saved[fd] = ft_strdup(buffer);
+		else
+			ft_joinandclean(saved, buffer, fd);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (ft_savestring(line, saved, fd, ret));
+}

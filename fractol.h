7078b1d8ef9afa262a21b6ca59cddbdada88ea07
Eero Vanha-Perttula <eero.vanhaperttula@gmail.com>
2022-10-13 20:34:55 +0300

    added global variables IMG_W and IMG_H to fractol.h

--- fractol.h
+++ fractol.h
@@ -6,16 +6,19 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 18:35:58 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/10/13 19:07:02 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/10/13 20:34:24 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FRACTOL_H
+#ifndef FRACTOL_H
 # define FRACTOL_H
 
 # include "..libft/includes/libft.h"
 # include <stdlib.h>
-#include <math.h>
-#include <fcntl.h>
+# include <math.h>
+# include <fcntl.h>
+
+# define IMG_W 2000
+# define IMG_H 2000
 
 #endif

3245a010cd7e828db1d9bdf8f9f168221c58b038
Eero Vanha-Perttula <eero.vanhaperttula@gmail.com>
2022-10-19 14:22:56 +0300

    added button press esc to exit the program

--- srcs/events.c
+++ srcs/events.c
@@ -6,16 +6,23 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 13:03:18 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/10/19 13:34:17 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/10/19 14:17:32 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+int		button_press(int button, t_fract *fract)
+{
+	if (button == ESC)
+		close_program(&fract->mlx);
+	return (0);
+}
+
 void	events(t_fract *fract)
 {
 	mlx_hook(fract->mlx.win_ptr, 4, 0, mouse_button, fract);
 	mlx_hook(fract->mlx.win_ptr, 6, 0, mouse_button, fract);
-	/*mlx_hook(fract->mlx.win_ptr, 6, 0, mouse_movement, fract);*/
-	/*mlx_hook(fract->mlx.win_ptr, 1L << 0, button_press, fract);*/
+	mlx_hook(fract->mlx.win_ptr, 6, 0, mouse_movement, fract);
+	mlx_hook(fract->mlx.win_ptr, 2, 0, button_press, fract);
 }

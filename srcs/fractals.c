57926b2ee4170b2171ae4193e3cc7f045639873d
Eero Vanha-Perttula <eero.vanhaperttula@gmail.com>
2022-10-19 11:41:24 +0300

    minor changes

--- srcs/fractals.c
+++ srcs/fractals.c
@@ -6,10 +6,38 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 18:40:03 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/10/13 19:07:01 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/10/19 11:31:12 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int		mandelbrot(
+int		mandelbrot(double c_re, double c_img)
+{
+	double z_re;
+	double z_img;
+	double temp;
+	int n;
+
+	z_re = c_re;
+	z_img = c_img;
+	n = 0;
+	while (n < MAX_ITER)
+	{
+		if ((z_img * z_img + z_re * z_re) > 4)
+			return (n);
+		temp = z_img;
+		z_img = z_re * z_img + z_re * z_img + c_img;
+		z_re = z_re * z_re + temp * temp * (-1) + c_re;
+		n++;
+	}
+	return (0);
+}
+
+int		picker(int fractal, double c_re, double c_img)
+{
+	if (fractal == MANDELBROT)
+		return (mandelbrot(c_re, c_img));
+	else
+		return (0);
+}
--- srcs/init.c
+++ srcs/init.c
@@ -6,9 +6,11 @@
 /*   By: evanha-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 11:10:50 by evanha-p          #+#    #+#             */
-/*   Updated: 2022/10/19 11:16:25 by evanha-p         ###   ########.fr       */
+/*   Updated: 2022/10/19 11:26:32 by evanha-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "fractol.h"
 
 void	init_values(t_fract *fract)
 {
